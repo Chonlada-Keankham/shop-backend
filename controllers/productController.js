75d0548c9170b1b1338d3e4ae1016abe5f1e8e6f
Chonlada Keankham <chonlada.kkc@gmail.com>
2026-03-21 16:03:43 +0700

    .

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 // -------------------------------------------------------------------
 // GET ALL PRODUCTS (PUBLIC)
@@ -176,12 +176,4 @@ exports.deleteProduct = (req, res) => {
       message: "Product deleted successfully",
     });
   });
-};
-
-module.exports = {
-  createProduct,
-  getAllProducts,
-  getProductById,
-  patchProduct,
-  deleteProduct,
 };
