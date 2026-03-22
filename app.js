52af8c1f9a6b5cd2b1fddb84c8e782d5cdde4184
Chonlada Keankham <chonlada.kkc@gmail.com>
2026-03-22 09:43:26 +0700

    rout

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const db = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
-
-// ยังไม่เปิดใช้ชั่วคราว เพราะยังไม่ได้แปลงจาก MySQL -> PostgreSQL ให้ครบ
-// const authRoutes = require('./routes/authRoutes');
-// const cartRoutes = require('./routes/cartRoutes');
-// const cartItemRoutes = require('./routes/cartItemRoutes');
-// const orderRoutes = require("./routes/orderRouter");
-// const orderItemRoutes = require("./routes/orderItemRoutes");
 
 const app = express();
 
@@ -33,15 +27,8 @@ app.get('/test-db', async (req, res) => {
   }
 });
 
-// เปิดใช้เฉพาะ route ที่แปลงเป็น PostgreSQL แล้ว
+app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
-
-// ยังไม่เปิดใช้ชั่วคราว
-// app.use('/api/auth', authRoutes);
-// app.use('/api/carts', cartRoutes);
-// app.use('/api/cart-items', cartItemRoutes);
-// app.use("/api/orders", orderRoutes);
-// app.use("/api/order-items", orderItemRoutes);
 
 const PORT = process.env.PORT || 5000;
 
